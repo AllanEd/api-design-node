6f699a29a02e0384957852ac6bc8cd3082aefc41
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-08-09 19:56:52 +0200

    Exercise 1 - my version

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var fs = require('fs');
 
 var jsonData = {count: 12, message: 'hey'};
+
+var app = express();
+
+app.get('/', function(req, res) {
+  var filePath = "./index.html";
+  fs.createReadStream(filePath).pipe(res);
+});
+
+app.get('/data', function(req, res) {
+  res.json(jsonData);
+});
+
+app.listen(3000);
