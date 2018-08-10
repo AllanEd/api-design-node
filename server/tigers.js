7fc97e67287ae98ea091de29cd2c667e1a0c7eb7
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-08-10 10:03:01 +0200

    Exercice 4

--- server/tigers.js
+++ server/tigers.js
@@ -2,3 +2,61 @@
 // and make some REST routes for it, exactly like for lions
 // make a middleware that just logs the word 'tiger' to the console
 // when a request comes in to the server
+var tigerRouter = require('express').Router();
+
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+  if (!req.body.id) {
+    id++;
+    req.body.id = id + '';
+  }
+  next();
+};
+
+tigerRouter.param('id', function(req, res, next, id) {
+  var todo = _.find(todos, {id: id});
+
+  if (todo) {
+    req.todo = todo;
+    next();
+  } else {
+    res.send();
+  }
+});
+
+tigerRouter.get('/', function(req, res){
+  res.json(lions);
+});
+
+tigerRouter.get('/:id', function(req, res){
+  var lion = req.todo;
+  res.json(lion || {});
+});
+
+tigerRouter.post('/', updateId, function(req, res) {
+  var lion = req.body;
+
+  lions.push(lion);
+
+  res.json(lion);
+});
+
+
+tigerRouter.put('/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
+});
+
+module.exports = tigerRouter;
